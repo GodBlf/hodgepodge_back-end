bf4ddc0799c3e6c3aa982cf4d71c04612287736a
godblf <yyilin969@gmail.com>
2025-09-09 00:31:06 +0800

    execution to deviceid

--- utils/util.go
+++ utils/util.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"errors"
+	"strings"
+)
+
+func Exe2DeviceID(execution string) (string, error) {
+	index := strings.Index(execution, "_")
+	if index == -1 {
+		return "", errors.New("execution格式错误")
+	}
+	return execution[0:index], nil
+}
